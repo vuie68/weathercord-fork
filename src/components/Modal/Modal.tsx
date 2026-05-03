ac6ae4017d91b7e7ed29a6d9bfa454f4451e0867
raynecloudy <hi@rayne.page>
2026-05-03 01:42:55 -0700

    strengthen modal background effects

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,9 +5,9 @@ const Modal = (props: Record<string, any> & {
   children: ReactNode
 }) => {
   return (
-    <div className="backdrop-blur-xs backdrop-brightness-50 w-screen h-screen p-2 absolute top-0 left-0 flex items-center justify-center">
+    <div className="backdrop-blur-sm backdrop-brightness-40 w-screen h-screen p-2 absolute top-0 left-0 flex items-center justify-center">
       <Box className={"p-2 rounded-2xl w-30 " + props.className} style={{
-        boxShadow: "0 0.5rem 2rem #000b"
+        boxShadow: "0 0.5rem 2rem #000000bb"
       }}>{props.children}</Box>
     </div>
   );
